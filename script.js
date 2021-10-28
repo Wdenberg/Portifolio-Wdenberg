f45ad498268da69201c75220c169b5f0ea6d9b19
WDENBERG RAMOS DE BARROS <88355939+Wdenberg@users.noreply.github.com>
2021-10-27 22:40:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["/Font-End", "/Freelancer"],
+        strings: ["/Front-End", "/Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["/Font-End", "/Freelancer"],
+        strings: ["/Front-End", "/Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
